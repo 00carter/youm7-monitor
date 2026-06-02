4e9919c918ebc622c5d4ba1596d05662289c1550
Aly <00carter@users.noreply.github.com>
2026-06-02 21:31:16 +0300

    Fix multiple bugs

--- src/status.py
+++ src/status.py
@@ -7,11 +7,12 @@ import os
 
 logging.basicConfig(level=logging.INFO)
 
-if not os.path.exists("memory.html"):
-    open("memory.html", "w", encoding="utf-8").close()
+url = "https://www.youm7.com/home/"
+
 
 def main():
-    url = "https://www.youm7.com/home/"
+    check_memory()
+
     articles = []
     load(articles)
 
@@ -22,31 +23,48 @@ def main():
 
         with open("memory.html", "r+", encoding="utf-8") as f:
             if f.read() != html:
-                logging.warning("Something Changed!") #Debug
+                logging.warning("Something Changed!")  # Debug
 
                 clear_file(f)
                 f.write(html)
 
                 load(articles)
-                print(f"📰 New! \n Title: {articles[0]["title"]}\n Link: https://www.youm7.com/{articles[0]["link"]}\n Date: {datetime.now()}\n")
-        
+
+                print(
+                    f"📰 New! \n Title: {articles[0]["title"]}\n Link: https://www.youm7.com{articles[0]["link"]}\n Date: {datetime.now()}\n"
+                )
+
         time.sleep(10)
+
 
 def clear_file(f):
     f.seek(0)
     f.truncate()
-    logging.info("File Cleared!") #Debug
+    logging.info("File Cleared!")  # Debug
+
 
 def load(articles):
+    articles.clear()
+
     with open("memory.html", "r+", encoding="utf-8") as f:
         soup_memory = BeautifulSoup(f.read(), "lxml")
         articles_html = soup_memory.find_all("div", class_="label1")
-    
+
     for article in articles_html:
         title = article.a.text.strip()
         link = article.a["href"].strip()
         articles.append({"title": title, "link": link})
-    
-    logging.info("Articles Loaded!") #Debug
+
+    logging.info("Articles Loaded!")  # Debug
+
+
+def check_memory():
+    if not os.path.exists("memory.html"):
+        r = requests.get(url).text
+        soup = BeautifulSoup(r, "lxml")
+        html = soup.find("div", id="mainNews").prettify()
+        with open("memory.html", "w", encoding="utf-8") as f:
+            f.write(html)
+
 
 main()
